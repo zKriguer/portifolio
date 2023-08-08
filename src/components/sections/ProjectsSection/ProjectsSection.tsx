11e129497ceea9d253ab0e1a412909b6501036d0
zKriguer <kriguerfreitas2020@gmail.com>
2023-08-08 03:38:21 -0300

    fix: typerror fix

--- src/components/sections/ProjectsSection/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection/ProjectsSection.tsx
@@ -1,30 +1,43 @@
 import { GitHubRepository } from "@/lib/types/GithubRepository";
-
 import { GithubApiService } from "@/lib/utils";
 import { getScopedI18n } from "@/app/locales/server";
-
 import { ProjectCards } from "./ProjectCards";
 
 export const ProjectsSection = async () => {
   const t = await getScopedI18n("navigation");
 
-  const repos: GitHubRepository[] = await GithubApiService(
-    "https://api.github.com/users/zKriguer/repos"
-  ).then((res) =>
-    res?.map((repo: GitHubRepository) => {
-      if (repo != undefined && repo.topics.includes("projects")) {
-        return repo != undefined && repo;
-      }
-    })
-  );
+  try {
+    const res = await GithubApiService(
+      "https://api.github.com/users/zKriguer/repos"
+    );
 
-  repos.sort((a, b) => {
-    return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
-  });
+    if (!Array.isArray(res)) {
+      console.error("GitHub API response is not an array:", res);
+      return null;
+    }
 
-  return (
-    <div className="flex items-center w-full h-full justify-around gap-16 flex-col xl:flex-row flex-shrink">
-      <ProjectCards repos={repos} />
-    </div>
-  );
+    const filteredRepos: GitHubRepository[] = res.filter(
+      (repo: GitHubRepository) =>
+        repo != undefined && repo.topics.includes("projects")
+    );
+
+    filteredRepos.sort((a, b) => {
+      return (
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      );
+    });
+
+    return (
+      <div className="flex items-center w-full h-full justify-around gap-16 flex-col xl:flex-row flex-shrink">
+        <ProjectCards repos={filteredRepos} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching or processing GitHub repositories:", error);
+    return (
+      <div>
+        An error occurred while fetching or processing GitHub repositories.
+      </div>
+    );
+  }
 };
